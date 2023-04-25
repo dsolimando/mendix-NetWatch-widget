d7da4ede94bc4973d5603e50660630d41d0e6589
Damien <damien.solimando.ext@proximus.com>
2023-04-25 13:33:55 +0200

    fixes

--- src/NetWatchWidget.tsx
+++ src/NetWatchWidget.tsx
@@ -1,23 +1,35 @@
-import {createElement, ReactElement, useCallback} from "react";
-import {NetWatchProps} from "../typings/NetWatchProps";
-import {Netwatch} from "react-native-netwatch";
+import { createElement, ReactElement, useCallback, useEffect, useState } from "react";
+import { NetWatchProps } from "../typings/NetWatchProps";
+import { Netwatch } from "react-native-netwatch";
+import { ValueStatus } from "mendix";
 
 export function NetWatchWidget({ visible, onClose, enabled }: NetWatchProps<{}>): ReactElement {
-
     const onCloseHandler = useCallback(() => {
         if (onClose && onClose.canExecute && !onClose.isExecuting) {
             onClose.execute();
         }
     }, [onClose]);
 
+    const [visibleState, setVisibleState] = useState(false);
+    const [enabledState, setEnabledState] = useState(true);
+
+    useEffect(() => {
+        if (visible.status === ValueStatus.Available && visible.value !== undefined) {
+            setVisibleState(visible.value);
+        }
+        if (visible.status === ValueStatus.Available && enabled?.value !== undefined) {
+            setEnabledState(enabled.value);
+        }
+    }, [visible.value, enabled.value]);
+
     return (
         <Netwatch
             onPressClose={() => {
-                onCloseHandler()
+                onCloseHandler();
             }}
             useReactotron={false}
-            visible={visible?.value}
-            enabled={!!enabled?.value}
+            visible={visibleState}
+            enabled={enabledState}
             interceptIOS={true}
         />
     );
